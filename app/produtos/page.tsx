252e1092c26d9e73d8f458c12c9a048b907dc569
joanaclaudio <a22100503@alunos.ulht.pt>
2024-12-09 21:32:33 +0000

    Update

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,43 +1,54 @@
 'use client';
 
 import React from 'react';
-
-import { Produto } from '@/app/models/interfaces';
 import useSWR from 'swr';
+import { Produto } from '@/app/models/interfaces';
 
-// Função fetcher para buscar dados
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-// Componente para exibir os produtos
 const ProductsPage: React.FC = () => {
+  // Usa useSWR para consumir a API local
   const { data, error, isLoading } = useSWR<Produto[], Error>(
     '/api/products',
     fetcher
   );
 
+  // Validação dos estados
   if (error) return <div>Erro ao carregar os produtos.</div>;
   if (isLoading) return <div>Carregando produtos...</div>;
   if (!data || data.length === 0) return <div>Sem produtos disponíveis.</div>;
 
+  // Renderização dos produtos
   return (
     <div>
+      <h1>Lista de Produtos</h1>
       <ul>
         {data.map((produto) => (
-          <li key={produto.id}>{produto.title}</li>
-          /*colocar o resto dos atributos*/
+          
+          <li key={produto.id} style={{ margin: '1rem 0', padding: '1rem', border: '1px solid #ccc', borderRadius: '8px' }}>
+            <h2><strong>{produto.title}</strong></h2>
+            <img
+              src={produto.image}
+              alt={produto.title}
+              style={{ width: '150px', height: '150px', borderRadius: '4px'}}
+            />
+            
+            <p>Descrição: {produto.description}</p>
+            <p>Custo: €{produto.price.toFixed(2)}</p>
+            <p>Avaliação: {produto.rating.rate} (Baseado em {produto.rating.count} avaliações)</p>
+          </li>
         ))}
       </ul>
     </div>
-
   );
 };
 
-// Página principal que exibe os produtos no local desejado
+
 export default function Produtos() {
   return (
     <div>
       <h2>Produtos Disponíveis:</h2>
-      {/* Renderiza o componente ProductsPage aqui */}
+      
       <ProductsPage />
     </div>
   );
