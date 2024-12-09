bc0e63fccd47f70dd0dc699f9eeba6532cc4e8a1
joanaclaudio <a22100503@alunos.ulht.pt>
2024-12-09 17:55:55 +0000

    page

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return <>
+    <h1>React e Next.JS</h1>
+    <p>Bem-vindo á minha primeira app em react e next.js, 
+        das tecnologias Web mais usadas nos dias de hoje.</p>
+  
+  
+  </>
+    
+  
+}
