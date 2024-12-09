f96ed94292df00b42493405bed9ed22a7a6b7ca2
joanaclaudio <a22100503@alunos.ulht.pt>
2024-12-09 21:14:50 +0000

    UPDATE

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,15 +1,12 @@
+// app/api/products/route.ts
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    // Busca os produtos da API DEISI Shop
-    const response = await fetch('https://deisishop.pythonanywhere.com/products/');
-    const products = await response.json();
-
-    // Retorna os produtos em formato JSON
-    return NextResponse.json(products);
-  } catch (error) {
-    // Trata erros e retorna mensagem apropriada
-    return NextResponse.json({ error: 'Erro ao buscar os produtos.' }, { status: 500 });
+    const res = await fetch('https://deisishop.pythonanywhere.com/products/');
+    const produtos = await res.json();
+    return NextResponse.json(produtos);
+  } catch {
+    return NextResponse.json('Erro ao carregar produtos');
   }
 }
